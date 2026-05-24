dcb22345a000a69bd006051b499593b27dc351b9
raja <raja31.div@gmail.com>
2026-05-24 17:02:59 +0100

    add the event section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import ExploreBtn from "../components/ExploreBtn";
+import { events } from "../lib/constants";
+import EventCard from "../components/EventCard";
 export default function Page() {
   return (
-    <section>
+    <section className="w-full">
       <h1 className="text-center ">
         The Hub For Every Dev <br /> Event You Cant Miss
       </h1>
@@ -9,6 +11,16 @@ export default function Page() {
         Hackathons, Meetups, and Conferences, All in One Place
       </p>
       <ExploreBtn />
+      <div className="mt-20 w-screen relative left-1/2 -translate-x-1/2 px-5 sm:px-10">
+        <h3 className="text-left mb-8">Discover a World of Dev Events </h3>
+        <ul className="events">
+          {events.map((event) => (
+            <li key={event.slug} className="list-none">
+              <EventCard {...event} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
---
+++ components/EventCard.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+interface Props {
+  slug: string;
+  image: string;
+  title: string;
+  location: string;
+  date: string;
+  time: string;
+}
+function EventCard({ title, location, date, time, slug, image }: Props) {
+  return (
+    <Link href={`/events/${slug}`} id="event-card">
+      <Image
+        src={image}
+        alt={title}
+        className="poster"
+        width={410}
+        height={300}
+      />
+      <div className="flex flex-row gap-2">
+        <Image src="/pin.svg" alt="location" width={14} height={14} />
+        <p>{location}</p>
+      </div>
+      <p className="title">{title}</p>
+      <div className="datetime">
+        <div >
+          <Image src="/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
+        </div>
+        <div >
+          <Image src="/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
+        </div>
+      </div>
+    </Link>
+  );
+}
+export default EventCard;
